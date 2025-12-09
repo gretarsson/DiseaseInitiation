995ebb047dda78248d7232fbc7c04b9bc1eb6989
gretarsson <cga323@gmail.com>
2025-12-09 16:08:49 -0500

    minor changes

--- scripts/playground.jl
+++ scripts/playground.jl
@@ -10,27 +10,16 @@ W = Matrix(CSV.read("data/Schaefer2018_200Parcels_CN.csv", DataFrame; header=fal
 N = size(W,1)
 
 # load data
+println("\nloading dataset...")
 FDG_matrix, amyloid_matrix, tau_matrix = nothing, nothing, nothing
-#FDG_matrix, amyloid_matrix, tau_matrix = load_dataset(:baseline_amyloid_tau)
-FDG_matrix, amyloid_matrix, tau_matrix = load_dataset(:FDG_amyloid_tau_longitudinal)
+FDG_matrix, amyloid_matrix, tau_matrix, subject_IDs = load_dataset(:FDG_amyloid_tau_longitudinal; centiloid_threshold=nothing)
+println("...using $(size(FDG_matrix,1)) subjects.\n")
 
-# drop missing rows
-FDG_matrix, amyloid_matrix, tau_matrix = drop_missing_rows(FDG_matrix, amyloid_matrix, tau_matrix)
-if FDG_matrix !== nothing
-    #FDG_matrix ./= maximum(FDG_matrix)  # normalize by median
-    # normalize each row between 0 and 1
-    for i in axes(FDG_matrix,1)
-        FDG_matrix[i, :] .= (FDG_matrix[i, :] .- minimum(FDG_matrix[i, :])) ./ (maximum(FDG_matrix[i, :]) - minimum(FDG_matrix[i, :]))
-    end
-end
-if amyloid_matrix !== nothing
-    #amyloid_matrix ./= maximum(amyloid_matrix)  # normalize by median
-    # normalize each row between 0 and 1
-    for i in axes(amyloid_matrix,1)
-        amyloid_matrix[i, :] .= (amyloid_matrix[i, :] .- minimum(amyloid_matrix[i, :])) ./ (maximum(amyloid_matrix[i, :]) - minimum(amyloid_matrix[i, :]))
-    end
-end
-
+# drop missing rows and normalize
+FDG_matrix, amyloid_matrix, tau_matrix, nonmissing_subj = drop_missing_rows(FDG_matrix, amyloid_matrix, tau_matrix)
+FDG_matrix = normalize_rows(FDG_matrix)
+amyloid_matrix = normalize_rows(amyloid_matrix)
+subject_IDs = subject_IDs[nonmissing_subj]  # keep track of subject IDs after dropping missing
 
 
 # random Laplacian and activity matrix
@@ -47,13 +36,13 @@ for i in axes(amyloid_matrix,1)
     init_timeseries = disease_initiation_timeseries(L, amyloid_M, FDG_M, ones(N), 1, 0, 1, 0, 0, tspan, Tn)
 
     # plot timeseries
-    t = range(tspan[1], tspan[2], length=Tn)
-    plot(t, init_timeseries',  # transpose so each trajectory is a series
-        xlabel = "Time",
-        ylabel = "u(t)",
-        title = "All regions",
-        lw = 2,
-        alpha = 0.6,
-        legend = false)
-    savefig("figures/timeseries_subjects_FDG/timeseries_$(i).png")  
+    #t = range(tspan[1], tspan[2], length=Tn)
+    #plot(t, init_timeseries',  # transpose so each trajectory is a series
+    #    xlabel = "Time",
+    #    ylabel = "u(t)",
+    #    title = "All regions",
+    #    lw = 2,
+    #    alpha = 0.6,
+    #    legend = false)
+    #savefig("figures/timeseries_subjects_FDG/timeseries_$(i).png")  
 end
